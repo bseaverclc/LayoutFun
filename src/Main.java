5971674a55ae687922b763aaa214dd05145307d1
Brian Seaver <bseaver@d155.org>
2022-12-10 16:05:26 -0600

    added gridBag fill

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,14 @@ public class Main {
         panel.setBackground(Color.BLUE);
        // panel.setLayout(new GridLayout(3,2));
        // panel.setLayout(new BorderLayout());
+        GridBagLayout gridBag = new GridBagLayout();
+
+
         panel.setLayout(new GridBagLayout());
         GridBagConstraints gc = new GridBagConstraints();
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.BOTH;
 
 
 
@@ -25,19 +31,22 @@ public class Main {
         JButton button5 = new JButton("Fifth");
         JButton button6 = new JButton("Sixth");
 
-        gc.gridx = 2;
-        gc.gridy = 3;
+        gc.gridx = 0;
+        gc.gridy = 0;
         panel.add(button1, gc);
 
-        gc.gridx = 3;
-        gc.gridy = 3;
-        panel.add(button3, gc);
-
-        gc.gridx = 2;
-        gc.gridy = 4;
-        gc.gridwidth = 2;
-        gc.fill = GridBagConstraints.BOTH;
+        gc.gridx = 1;
+        gc.gridy = 0;
         panel.add(button2, gc);
+
+
+
+        gc.gridx = 1;
+        gc.gridy = 1;
+        //gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        panel.add(button3, gc);
+        //gc.anchor = GridBagConstraints.CENTER;
+        //panel.add(button5, gc);
 
 
 
